0aa444f736b0b8e9d3ab0c9d8278bda403b6ec50
Dan Gross <dgross1@paychex.com>
2017-09-19 13:52:19 -0400

    Fine-tune turns with gyro sensor.

--- codeMonkeys/src/Movement.h
+++ codeMonkeys/src/Movement.h
@@ -1,5 +1,8 @@
 static int FULL_SPEED = 100;
 
+void debug_print(char * debug) {
+   writeDebugStream(debug);
+}
 void stop_movement() {
 	motor[rightMotor] = 0;
 	motor[leftMotor] = 0;
@@ -11,18 +14,22 @@ void stop_movement(int msec) {
 }
 
 void turn_right(int degree) {
-	nMotorEncoderTarget[leftMotor] = degree*4;
+	int startvalue = SensorValue[S2];
+
 	motor[rightMotor] = 0;		    // Motor on motorB is stopped at 0 power
-	motor[leftMotor]  = 40;			// Motor on motorC is run at full (100) power forward
-	while(nMotorRunState[leftMotor] != runStateIdle)  wait1Msec(1);
+	motor[leftMotor]  = 30;			// Motor on motorC is run at full (100) power forward
+	while(SensorValue[S2] - startvalue < (degree*10))  wait1Msec(1);
+	writeDebugStream("\nright rotation, start: %d, end %d, total %d\n",startvalue, SensorValue[S2], SensorValue[S2] - startvalue);
 	stop_movement();
 }
 
 void turn_left(int degree) {
-	nMotorEncoderTarget[rightMotor] = degree*4;
-  motor[rightMotor] = 40;		    // Motor on motorB is turned off
+
+	int startvalue = SensorValue[S2];
+  motor[rightMotor] = 30;		    // Motor on motorB is turned off
   motor[leftMotor]  = 0;					// Motor on motorC is turned off
-	while(nMotorRunState[rightMotor] != runStateIdle)  wait1Msec(1);
+  while(startvalue - SensorValue[S2]  < (degree*10))  wait1Msec(1);
+  	writeDebugStream("left rotation start: %d, end %d, total %d\n",startvalue, SensorValue[S2],startvalue - SensorValue[S2]);
 	stop_movement();
 }
 
@@ -34,8 +41,4 @@ void go_straight(int speed, int msec) {
 	go_straight(speed);
 	wait1Msec(msec);
 	stop_movement();
-}
-
-void debug_print(char * debug) {
-   writeDebugStream(debug);
 }
